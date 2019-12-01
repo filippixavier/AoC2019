cfe2ff2cc49866ea7ad534be66485aab26244a9e
Xavier Filippi <gueltir@gmail.com>
2019-12-01 17:04:01 +0100

    First day second star

--- src/days/day1.rs
+++ src/days/day1.rs
@@ -3,7 +3,6 @@ use std::fs;
 use std::path::Path;
 
 pub fn first_star() -> Result<(), Box<dyn Error + 'static>> {
-    println!("{:?}", std::env::current_exe());
     println!(
         "{}",
         fs::read_to_string(Path::new("./data/day1.txt"))? // Yeah, it mostly depends on where you call the exec, but oh well...
@@ -15,5 +14,20 @@ pub fn first_star() -> Result<(), Box<dyn Error + 'static>> {
 }
 
 pub fn second_star() -> Result<(), Box<dyn Error + 'static>> {
+    println!(
+        "Total fuel needed: {}",
+        fs::read_to_string(Path::new("./data/day1.txt"))?
+            .split('\n')
+            .map(|x| {
+                let mut added_fuel = x.trim().parse::<i32>().unwrap_or(0) / 3 - 2;
+                let mut total_fuel = 0;
+                while added_fuel > 0 {
+                    total_fuel += added_fuel;
+                    added_fuel = added_fuel / 3 - 2;
+                }
+                total_fuel
+            })
+            .fold(0, |acc, x| x + acc)
+    );
     Ok(())
 }
