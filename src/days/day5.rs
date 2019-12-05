7c17342c3f74822cac1bf2d72153e6af90f25765
Xavier Filippi <gueltir@gmail.com>
2019-12-05 16:30:21 +0100

    Fifth day, first star

---
+++ src/days/day5.rs
@@ -0,0 +1,27 @@
+use std::error::Error;
+use std::fs;
+use std::path::Path;
+
+use super::intcode::Intcode;
+
+fn prepare_file(input: String) -> Vec<i32> {
+    input
+        .trim()
+        .split(',')
+        .map(|x| (x.trim().parse::<i32>().unwrap_or(0)))
+        .collect::<Vec<_>>()
+}
+
+pub fn first_star() -> Result<(), Box<dyn Error + 'static>> {
+    let mem = prepare_file(fs::read_to_string(Path::new("./data/day5.txt"))?);
+    let mut int_machine = Intcode::new(mem, 1);
+
+    while int_machine.next_op() {}
+
+    println!("The final output is: {}", int_machine.output);
+    Ok(())
+}
+
+pub fn second_star() -> Result<(), Box<dyn Error + 'static>> {
+    Ok(())
+}
