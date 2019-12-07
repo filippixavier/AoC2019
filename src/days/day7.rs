e0e117d1bf3cea65ac43d2dd9f0d004ecb55b82a
Xavier Filippi <gueltir@gmail.com>
2019-12-07 18:35:38 +0100

    Seventh day second star

--- src/days/day7.rs
+++ src/days/day7.rs
@@ -3,7 +3,7 @@ use std::error::Error;
 use std::fs;
 use std::path::Path;
 
-use super::intcode::Intcode;
+use super::intcode::{CompStatus, Intcode};
 
 fn prepare_file(input: String) -> Vec<i32> {
     input
@@ -75,5 +75,55 @@ pub fn first_star() -> Result<(), Box<dyn Error + 'static>> {
 }
 
 pub fn second_star() -> Result<(), Box<dyn Error + 'static>> {
+    let memory = prepare_file(fs::read_to_string(Path::new("./data/day7.txt"))?);
+
+    let permutations_it = (5..10).permutations(5);
+
+    let mut max_thruster: Option<i32> = None;
+    let mut permutation_str: String = String::from("None");
+
+    for permutation in permutations_it {
+        let current_permutation_str = permutation.iter().map(|elem| elem.to_string()).collect();
+
+        let mut first_amp = Intcode::new(memory.clone()).add_input(permutation[0]).run();
+
+        let mut second_amp = Intcode::new(memory.clone()).add_input(permutation[1]).run();
+        let mut third_amp = Intcode::new(memory.clone()).add_input(permutation[2]).run();
+
+        let mut fourth_amp = Intcode::new(memory.clone()).add_input(permutation[3]).run();
+
+        let mut fifth_amp = Intcode::new(memory.clone()).add_input(permutation[4]).run();
+
+        let mut input = 0;
+
+        while fifth_amp.status != CompStatus::Halted {
+            first_amp = first_amp.add_input(input).run();
+            second_amp = second_amp.add_input(first_amp.output).run();
+            third_amp = third_amp.add_input(second_amp.output).run();
+            fourth_amp = fourth_amp.add_input(third_amp.output).run();
+            fifth_amp = fifth_amp.add_input(fourth_amp.output).run();
+
+            input = fifth_amp.output;
+        }
+
+        max_thruster = if let Some(current_max) = max_thruster {
+            if current_max < input {
+                permutation_str = current_permutation_str;
+                Some(input)
+            } else {
+                Some(current_max)
+            }
+        } else {
+            permutation_str = current_permutation_str;
+            Some(input)
+        }
+    }
+
+    println!(
+        "Max thruster: {} for permutation: {}",
+        max_thruster.unwrap_or(-1),
+        permutation_str
+    );
+
     Ok(())
 }
