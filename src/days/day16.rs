2c5acff53a7c4e92db7a8fd97f8f316cb071941b
Xavier Filippi <gueltir@gmail.com>
2019-12-18 01:22:56 +0100

    Attempt at second star

--- src/days/day16.rs
+++ src/days/day16.rs
@@ -31,7 +31,6 @@ pub fn first_star() -> Result<(), Box<dyn Error + 'static>> {
                 .take(input_len)
                 .enumerate()
             {
-                // println!("{}, {} ", *value, current_input[index]);
                 next_value += *value * current_input[index];
             }
             new_input.push(next_value.abs() % 10);
@@ -50,5 +49,54 @@ pub fn first_star() -> Result<(), Box<dyn Error + 'static>> {
 }
 
 pub fn second_star() -> Result<(), Box<dyn Error + 'static>> {
+    let current_input = fs::read_to_string(Path::new("./data/day16.txt"))?;
+    let message_offset = current_input.chars().take(7).collect::<String>().parse::<usize>().unwrap();
+    let mut current_input = current_input
+        .chars()
+        .map(|elem| elem.to_digit(10).unwrap() as isize)
+        .collect::<Vec<isize>>();
+
+    let base_pattern = vec![0, 1, 0, -1];
+    let mut input_len = current_input.len();
+
+    current_input = current_input.into_iter().cycle().take(1_000 * input_len).collect();
+    input_len = current_input.len();
+
+    for _ in 0..1 {
+        let mut new_input = vec![];
+
+        for i in 1..=input_len {
+            let mut next_value = 0;
+            let mut current_pattern = vec![];
+            for elem in base_pattern.iter() {
+                for _ in 0..i {
+                    current_pattern.push(*elem);
+                }
+            }
+
+            for (index, value) in current_pattern
+                .iter()
+                .cycle()
+                .skip(1)
+                .take(input_len)
+                .enumerate()
+            {
+                next_value += *value * current_input[index];
+            }
+            new_input.push(next_value.abs() % 10);
+        }
+
+        current_input = new_input;
+    }
+
+    println!("{:?}", current_input);
+
+    let result = current_input
+        .iter()
+        .skip(message_offset)
+        .take(8)
+        .fold(String::new(), |acc, elem| acc + &elem.to_string());
+
+    println!("{}", result);
     Ok(())
 }
