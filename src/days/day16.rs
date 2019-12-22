04566fe48c400103564c620954e974deb3ccb132
Xavier Filippi <gueltir@gmail.com>
2019-12-22 01:09:11 +0100

    Sixteenth day, second star! (finally)

--- src/days/day16.rs
+++ src/days/day16.rs
@@ -9,34 +9,35 @@ pub fn first_star() -> Result<(), Box<dyn Error + 'static>> {
         .map(|elem| elem.to_digit(10).unwrap() as isize)
         .collect::<Vec<isize>>();
 
-    let base_pattern = vec![0, 1, 0, -1];
-    let input_len = current_input.len();
-
     for _ in 0..100 {
-        let mut new_input = vec![];
+        let mut next_input = vec![];
 
-        for i in 1..=input_len {
+        for i in 1..=current_input.len() / 2 {
             let mut next_value = 0;
-            let mut current_pattern = vec![];
-            for elem in base_pattern.iter() {
-                for _ in 0..i {
-                    current_pattern.push(*elem);
-                }
+
+            for (index, value) in current_input.iter().enumerate() {
+                let position = ((index + 1) as f32 / i as f32).trunc();
+                let pattern_value =
+                    ((position * std::f32::consts::FRAC_PI_2).sin()).round() as isize;
+
+                next_value += pattern_value * value;
             }
 
-            for (index, value) in current_pattern
-                .iter()
-                .cycle()
-                .skip(1)
-                .take(input_len)
-                .enumerate()
-            {
-                next_value += *value * current_input[index];
-            }
-            new_input.push(next_value.abs() % 10);
+            next_input.push(next_value.abs() % 10);
         }
 
-        current_input = new_input;
+        let mut sum: isize = current_input
+            .iter()
+            .skip(current_input.len() / 2)
+            .cloned()
+            .sum::<isize>();
+
+        for value in current_input.iter().skip(current_input.len() / 2) {
+            next_input.push(sum % 10);
+            sum -= value;
+        }
+
+        current_input = next_input;
     }
 
     let result = current_input
@@ -48,55 +49,54 @@ pub fn first_star() -> Result<(), Box<dyn Error + 'static>> {
     Ok(())
 }
 
+// Solution HEAVILY influenced by this Reddit Thread: https://www.reddit.com/r/adventofcode/comments/ebf5cy/2019_day_16_part_2_understanding_how_to_come_up/
+// Also remember using partial sum (https://github.com/enjmusic/aoc_2019/blob/master/aoc_16/src/main.rs => apply_fft)
 pub fn second_star() -> Result<(), Box<dyn Error + 'static>> {
-    let current_input = fs::read_to_string(Path::new("./data/day16.txt"))?;
-    let message_offset = current_input.chars().take(7).collect::<String>().parse::<usize>().unwrap();
-    let mut current_input = current_input
+    let input = fs::read_to_string(Path::new("./data/day16.txt"))?;
+    let message_offset = input
+        .chars()
+        .take(7)
+        .collect::<String>()
+        .parse::<usize>()
+        .unwrap();
+    let input: Vec<_> = input
         .chars()
         .map(|elem| elem.to_digit(10).unwrap() as isize)
+        .collect();
+    let initial_len = input.len();
+
+    let mut input: Vec<_> = input
+        .iter()
+        .cycle()
+        .take(initial_len * 10_000)
+        .cloned()
+        .collect();
+    input = input
+        .into_iter()
+        .skip(message_offset)
         .collect::<Vec<isize>>();
 
-    let base_pattern = vec![0, 1, 0, -1];
-    let mut input_len = current_input.len();
+    for _ in 0..100 {
+        let mut next_input = vec![];
 
-    current_input = current_input.into_iter().cycle().take(1_000 * input_len).collect();
-    input_len = current_input.len();
+        let mut sum: isize = input.iter().sum::<isize>();
 
-    for _ in 0..1 {
-        let mut new_input = vec![];
-
-        for i in 1..=input_len {
-            let mut next_value = 0;
-            let mut current_pattern = vec![];
-            for elem in base_pattern.iter() {
-                for _ in 0..i {
-                    current_pattern.push(*elem);
-                }
-            }
-
-            for (index, value) in current_pattern
-                .iter()
-                .cycle()
-                .skip(1)
-                .take(input_len)
-                .enumerate()
-            {
-                next_value += *value * current_input[index];
-            }
-            new_input.push(next_value.abs() % 10);
+        for item in input {
+            next_input.push(sum % 10);
+            sum = (sum - item).abs();
         }
 
-        current_input = new_input;
+        input = next_input;
     }
 
-    println!("{:?}", current_input);
+    println!(
+        "{}",
+        input
+            .into_iter()
+            .take(8)
+            .map(|x| x.to_string())
+            .collect::<String>()
+    );
 
-    let result = current_input
-        .iter()
-        .skip(message_offset)
-        .take(8)
-        .fold(String::new(), |acc, elem| acc + &elem.to_string());
-
-    println!("{}", result);
     Ok(())
 }
