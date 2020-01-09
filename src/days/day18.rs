c3f58c6e44454c8b794865f19813382ade297b26
Xavier Filippi <gueltir@gmail.com>
2020-01-09 03:02:40 +0100

    Reworking main algorithm

--- src/days/day18.rs
+++ src/days/day18.rs
@@ -16,7 +16,7 @@ enum Tile {
     Wall,
     Door(char),
     Key(char),
-    Start,
+    Start(char),
 }
 
 use Tile::*;
@@ -25,8 +25,9 @@ use Tile::*;
 struct Node {
     lockers: HashSet<char>,
     keys: HashSet<char>,
+    dist: usize,
+    is_key: bool,
     neighbours: HashMap<char, usize>,
-    targets: HashMap<char, usize>,
 }
 
 impl Node {
@@ -34,8 +35,9 @@ impl Node {
         Node {
             lockers: HashSet::new(),
             keys: HashSet::new(),
+            dist: 0,
+            is_key: false,
             neighbours: HashMap::new(),
-            targets: HashMap::new(),
         }
     }
 }
@@ -45,15 +47,16 @@ impl Clone for Node {
         Node {
             lockers: self.lockers.clone(),
             neighbours: self.neighbours.clone(),
+            dist: self.dist,
+            is_key: self.is_key,
             keys: self.keys.clone(),
-            targets: HashMap::new(),
         }
     }
 }
 
 fn create_map(input: String) -> Map {
     let mut network: HashMap<(usize, usize), Tile> = HashMap::new();
-
+    let mut start_count = 0;
     for (line_no, line) in input.split('\n').enumerate() {
         let line = line.trim();
         for (col_no, character) in line.chars().enumerate() {
@@ -61,7 +64,11 @@ fn create_map(input: String) -> Map {
             let tile = match character {
                 '.' => Empty,
                 '#' => Wall,
-                '@' => Start,
+                '@' => {
+                    let id = start_count.to_string().chars().next().unwrap();
+                    start_count += 1;
+                    Start(id)
+                }
                 x => {
                     if x.is_ascii_uppercase() {
                         Door(x)
@@ -78,238 +85,234 @@ fn create_map(input: String) -> Map {
     network
 }
 
-fn find_bfs_star_1(map: &Map, num_of_keys: usize) -> usize {
-    let mut visitable = map
-        .iter()
-        .filter(|(_, val)| **val == Start)
-        .map(|(key, _)| (*key, 0, HashSet::new()))
-        .collect::<VecDeque<(Coordinate, usize, HashSet<char>)>>();
-
-    let mut visited: HashSet<String> = HashSet::new();
-
-    let direction = vec![(1, 0), (-1, 0), (0, 1), (0, -1)];
-
-    loop {
-        if visitable.is_empty() {
-            panic!("Cannot move further yet haven't found all the keys");
-        }
-        let (current_pos, current_dist, keys_list) = visitable.pop_front().unwrap();
-        let mut keys_list_sorted = keys_list.iter().copied().collect::<Vec<char>>();
-        keys_list_sorted.sort();
-
-        let signature = format!(
-            "{}_{}_{}",
-            current_pos.0,
-            current_pos.1,
-            keys_list_sorted.iter().collect::<String>()
-        );
-
-        if !visited.insert(signature.clone()) {
-            continue;
-        }
-
-        for movement in direction.iter() {
-            let next_tile_coordinate = (
-                (current_pos.0 as isize + movement.0) as usize,
-                (current_pos.1 as isize + movement.1) as usize,
-            );
-
-            let tile = map.get(&next_tile_coordinate).unwrap();
-            match tile {
-                Key(key) => {
-                    let mut keys_list = keys_list.clone();
-                    keys_list.insert(*key);
-                    if keys_list.len() == num_of_keys {
-                        return current_dist + 1;
-                    }
-                    visitable.push_back((next_tile_coordinate, current_dist + 1, keys_list));
-                }
-                Door(lock) => {
-                    let required_key = lock.to_ascii_lowercase();
-                    if keys_list.contains(&required_key) {
-                        visitable.push_back((
-                            next_tile_coordinate,
-                            current_dist + 1,
-                            keys_list.clone(),
-                        ));
-                    }
-                }
-                Wall => {}
-                _ => {
-                    visitable.push_back((
-                        next_tile_coordinate,
-                        current_dist + 1,
-                        keys_list.clone(),
-                    ));
-                }
-            }
-        }
-    }
-}
-
-fn bfs_map_to_graph(map: &Map) -> Graph {
+fn bfs_from_node(map: &Map, start: Coordinate, is_root: bool, graph: &mut Graph) {
     use std::iter::FromIterator;
-    let mut graph = Graph::new();
-    let mut to_visit = map
-        .iter()
-        .filter(|(_, val)| **val == Start)
-        .enumerate()
-        .map(|(index, (key, _))| {
-            (
-                *key,
-                0,
-                vec![],
-                format!("{}", index).chars().next().unwrap(),
-            )
-        })
-        .collect::<VecDeque<(Coordinate, usize, Vec<char>, char)>>();
+    let mut to_visit: VecDeque<(Coordinate, usize, usize)> = VecDeque::new();
+    to_visit.push_back((start, 0, 0));
 
-    let direction = vec![(1, 0), (-1, 0), (0, 1), (0, -1)];
-    let mut already_visited: HashSet<Coordinate> = HashSet::new();
+    let mut already_discovered: HashSet<Coordinate> = HashSet::new();
+    already_discovered.insert(start);
+
+    let mut lock_states = vec![String::new()];
+
+    let movements = vec![(1, 0), (-1, 0), (0, 1), (0, -1)];
+
+    let node_id = match map.get(&start).unwrap() {
+        Start(id) | Key(id) => *id,
+        _ => unreachable!(),
+    };
+
+    let mut node = Node::new();
+    if let Some(previous_node) = graph.get(&node_id) {
+        node.lockers = previous_node.lockers.clone();
+    }
+
     while !to_visit.is_empty() {
-        let (current_pos, current_dist, current_locks, origin_node) = to_visit.pop_front().unwrap();
-        let next_dist = current_dist + 1;
-        for movement in direction.iter() {
-            let next_pos = (
-                (current_pos.0 as isize + movement.0) as usize,
-                (current_pos.1 as isize + movement.1) as usize,
+        let (coordinate, mut distance, mut lock_state_id) = to_visit.pop_back().unwrap();
+
+        distance += 1;
+
+        for movement in movements.iter() {
+            let neighbour_tile = (
+                (coordinate.0 as isize + movement.0) as usize,
+                (coordinate.1 as isize + movement.1) as usize,
             );
 
-            if !already_visited.insert(next_pos) {
+            if !already_discovered.insert(neighbour_tile) {
                 continue;
             }
 
-            match map.get(&next_pos).unwrap() {
+            match map.get(&neighbour_tile).unwrap() {
+                Start(id) | Key(id) => {
+                    node.neighbours.insert(*id, distance);
+
+                    if is_root {
+                        let mut remote_node = graph.entry(*id).or_insert_with(Node::new);
+                        remote_node.lockers =
+                            HashSet::from_iter(lock_states[lock_state_id].chars());
+                    }
+
+                    let new_lock_state =
+                        format!("{}{}", lock_states[lock_state_id], id.to_ascii_lowercase());
+                    lock_states.push(new_lock_state);
+                    lock_state_id = lock_states.len() - 1;
+
+                    to_visit.push_front((neighbour_tile, distance, lock_state_id));
+                }
+                Door(id) => {
+                    let new_lock_state =
+                        format!("{}{}", lock_states[lock_state_id], id.to_ascii_lowercase());
+                    lock_states.push(new_lock_state);
+                    lock_state_id = lock_states.len() - 1;
+
+                    to_visit.push_front((neighbour_tile, distance, lock_state_id));
+                }
+                Empty => {
+                    to_visit.push_front((neighbour_tile, distance, lock_state_id));
+                }
                 Wall => {}
-                Door(lock) => {
-                    let mut new_locks = current_locks.clone();
-                    new_locks.push(lock.to_ascii_lowercase());
-                    to_visit.push_back((next_pos, next_dist, new_locks, origin_node))
-                }
-                Key(node_name) => {
-                    let previous_node = graph.entry(origin_node).or_insert(Node::new());
-                    previous_node.neighbours.insert(*node_name, next_dist);
-
-                    let mut node = Node::new();
-                    node.lockers = HashSet::from_iter(current_locks.iter().copied());
-                    node.neighbours.insert(origin_node, next_dist);
-                    graph.insert(*node_name, node);
-
-                    to_visit.push_back((next_pos, 0, vec![], *node_name));
-                }
-                _ => {
-                    to_visit.push_back((next_pos, next_dist, current_locks.clone(), origin_node));
-                }
             }
         }
     }
 
-    graph
+    graph.insert(node_id, node);
 }
 
-fn bfs_graph_to_star(graph: &Graph) {
-    let mut queue: VecDeque<Node> = graph
+fn bfs_graph_to_star(graph: &Graph) -> usize {
+    let mut min = None;
+
+    let mut dedup: HashMap<String, usize> = HashMap::new();
+
+    let mut queue: VecDeque<(Node, char)> = graph
         .iter()
         .filter(|(key, _)| key.is_ascii_digit())
-        .map(|(_, start)| {let start = start.clone();
-        start.targets = start.neighbours.clone()
-    start})
+        .map(|(key, start)| (start.clone(), *key))
         .collect();
-    
     while !queue.is_empty() {
-        let node = queue.pop_front().unwrap();
+        let (node, last_visited) = queue.pop_front().unwrap();
+        if node.keys.len() == graph.len() {
+            min = if let Some(min_value) = min {
+                Some(std::cmp::min(min_value, node.dist))
+            } else {
+                Some(node.dist)
+            }
+        }
+
+        let mut signature = node.keys.iter().copied().collect::<Vec<char>>();
+
+        signature.sort();
+        let signature = format!("{}{}", signature.iter().collect::<String>(), last_visited);
+
+        if let Some(dedup_min) = dedup.get(&signature) {
+            if *dedup_min <= node.dist {
+                continue;
+            }
+        }
+
+        dedup.insert(signature, node.dist);
+
+        for (node_name, dist) in node.neighbours.iter() {
+            let next_node = graph.get(node_name).unwrap();
+            if next_node.lockers.is_subset(&node.keys) && !node.keys.contains(node_name) {
+                let mut new_node = next_node.clone();
+                new_node.dist = node.dist + dist;
+                new_node.keys = node.keys.clone();
+                new_node.keys.insert(*node_name);
+                queue.push_back((new_node, *node_name));
+            }
+        }
     }
+
+    min.unwrap_or(0)
 }
 
-fn find_bfs_star_2(map: &Map, num_of_keys: usize) -> usize {
-    let start_points = map
+fn bfs_graph_to_star_2(graph: &Graph) -> usize {
+    let mut min = None;
+
+    let mut dedup: HashMap<String, usize> = HashMap::new();
+
+    let roots: Vec<Node> = graph
         .iter()
-        .filter(|(_, val)| **val == Start)
-        .map(|(key, _)| *key)
-        .collect::<Vec<Coordinate>>();
+        .filter(|(key, _)| key.is_ascii_digit())
+        .map(|(_, start)| start.clone())
+        .collect();
 
-    let mut visited: HashSet<String> = HashSet::new();
+    let mut queue: VecDeque<(Vec<Node>, Option<char>)> = VecDeque::new();
+    queue.push_front((roots, None));
+    while !queue.is_empty() {
+        let (nodes, last_visited) = queue.pop_front().unwrap();
 
-    let mut visitable: VecDeque<(Vec<Coordinate>, usize, HashSet<char>)> = VecDeque::new();
-    visitable.push_front((start_points, 0, HashSet::new()));
+        let first_node = &nodes[0];
 
-    let direction = vec![(1, 0), (-1, 0), (0, 1), (0, -1)];
-
-    loop {
-        if visitable.is_empty() {
-            panic!("Cannot move further yet haven't found all the keys");
-        }
-        let (all_pos, current_dist, keys_list) = visitable.pop_front().unwrap();
-
-        let mut keys_list_sorted = keys_list.iter().copied().collect::<Vec<char>>();
-        keys_list_sorted.sort();
-
-        let signed_pos = all_pos.iter().fold(String::new(), |acc, pos| {
-            format!("{}_{}_{}", acc, pos.0, pos.1)
-        });
-
-        let signature = format!(
-            "{}_{}",
-            signed_pos,
-            keys_list_sorted.iter().collect::<String>()
-        );
-        if !visited.insert(signature.clone()) {
-            continue;
+        if first_node.keys.len() == graph.len() {
+            min = if let Some(min_value) = min {
+                Some(std::cmp::min(min_value, first_node.dist))
+            } else {
+                Some(first_node.dist)
+            }
         }
 
-        for (index, pos) in all_pos.iter().enumerate() {
-            for movement in direction.iter() {
-                let next_tile_coordinate = (
-                    (pos.0 as isize + movement.0) as usize,
-                    (pos.1 as isize + movement.1) as usize,
-                );
+        if let Some(last_char) = last_visited {
+            let mut signature = first_node.keys.iter().copied().collect::<Vec<char>>();
+            signature.sort();
+            let signature = format!("{}{}", signature.iter().collect::<String>(), last_char);
 
-                let mut new_pos = all_pos.clone();
-                new_pos[index] = next_tile_coordinate;
+            if let Some(dedup_min) = dedup.get(&signature) {
+                if *dedup_min <= first_node.dist {
+                    continue;
+                }
+            }
 
-                let tile = map.get(&next_tile_coordinate).unwrap();
-                match tile {
-                    Key(key) => {
-                        let mut keys_list = keys_list.clone();
-                        keys_list.insert(*key);
-                        if keys_list.len() == num_of_keys {
-                            return current_dist + 1;
-                        }
-                        visitable.push_back((new_pos, current_dist + 1, keys_list));
-                    }
-                    Door(lock) => {
-                        let required_key = lock.to_ascii_lowercase();
-                        if keys_list.contains(&required_key) {
-                            visitable.push_back((new_pos, current_dist + 1, keys_list.clone()));
+            dedup.insert(signature, first_node.dist);
+        }
+
+        for (index, node) in nodes.iter().enumerate() {
+            for (node_name, dist) in node.neighbours.iter() {
+                let next_node = graph.get(node_name).unwrap();
+                if next_node.lockers.is_subset(&node.keys) && !node.keys.contains(node_name) {
+                    let mut other_nodes = nodes.clone();
+                    for (index_other, other_node) in other_nodes.iter_mut().enumerate() {
+                        if index_other != index {
+                            other_node.dist = node.dist + dist;
+                            other_node.keys.insert(*node_name);
                         }
                     }
-                    Wall => {}
-                    _ => {
-                        visitable.push_back((new_pos, current_dist + 1, keys_list.clone()));
-                    }
+
+                    let mut new_node = next_node.clone();
+
+                    new_node.dist = node.dist + dist;
+                    new_node.keys = node.keys.clone();
+                    new_node.keys.insert(*node_name);
+
+                    other_nodes[index] = new_node;
+                    queue.push_back((other_nodes, Some(*node_name)));
                 }
             }
         }
     }
+
+    min.unwrap_or(0)
 }
 
 pub fn first_star() -> Result<(), Box<dyn Error + 'static>> {
     let map = create_map(fs::read_to_string(Path::new("./data/day18_a.txt"))?);
-    /*let result = find_bfs_star_1(&map, graph.len());
-    println!("Shortest path: {}", result);*/
-    let graph = bfs_map_to_graph(&map);
+    let mut graph = Graph::new();
 
-    for (key, val) in graph.iter() {
-        println!("{}: {:?}", key, val);
+    for (coord, tile) in map.iter().filter(|(_, tile)| match tile {
+        Key(_) | Start(_) => true,
+        _ => false,
+    }) {
+        let is_root = match tile {
+            Start(_) => true,
+            Key(_) => false,
+            _ => unreachable!(),
+        };
+        bfs_from_node(&map, *coord, is_root, &mut graph);
     }
+
+    println!("Min dist: {}", bfs_graph_to_star(&graph));
 
     Ok(())
 }
 
 pub fn second_star() -> Result<(), Box<dyn Error + 'static>> {
-    // let (map, graph) = create_map(fs::read_to_string(Path::new("./data/day18_b.txt"))?);
-    // let result = find_bfs_star_2(&map, graph.len());
-    // println!("Shortest path: {}", result);
+    let map = create_map(fs::read_to_string(Path::new("./data/day18_b.txt"))?);
+    let mut graph = Graph::new();
+
+    for (coord, tile) in map.iter().filter(|(_, tile)| match tile {
+        Key(_) | Start(_) => true,
+        _ => false,
+    }) {
+        let is_root = match tile {
+            Start(_) => true,
+            Key(_) => false,
+            _ => unreachable!(),
+        };
+        bfs_from_node(&map, *coord, is_root, &mut graph);
+    }
+
+    println!("Min dist: {}", bfs_graph_to_star_2(&graph));
 
     Ok(())
 }
+// 2478 too high
