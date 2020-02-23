0e3a975c58eeba135be9475bbd3474a767325b10
Xavier Filippi <gueltir@gmail.com>
2020-02-23 19:56:32 +0100

    Twenty-first day, second star

--- src/days/day21.rs
+++ src/days/day21.rs
@@ -44,5 +44,41 @@ pub fn first_star() -> Result<(), Box<dyn Error + 'static>> {
 }
 
 pub fn second_star() -> Result<(), Box<dyn Error + 'static>> {
+    let mut jumping_drone =
+        intcode::Intcode::new_with_path(fs::read_to_string(Path::new("./data/day21.txt"))?);
+    let program =
+        "OR A T\nAND B T\nAND C T\nNOT T J\nAND D J\nNOT E T\nNOT T T\nOR H T\nAND T J\nRUN\n";
+
+    let jmp_prog = program
+        .chars()
+        .map(|x| (x as u8) as i64)
+        .collect::<Vec<i64>>();
+
+    jumping_drone = jumping_drone.run();
+
+    jumping_drone.get_outputs();
+
+    for i in jmp_prog {
+        jumping_drone = jumping_drone.add_input(i);
+    }
+
+    jumping_drone = jumping_drone.run();
+
+    let mut output = jumping_drone.get_outputs();
+
+    output.reverse();
+
+    let text = output.iter().fold(String::new(), |mut acc, x| {
+        if *x < 256 {
+            let character = (*x as u8) as char;
+            acc.push(character);
+        } else {
+            return format!("{}{}", acc, x);
+        }
+
+        acc
+    });
+
+    println!("Result:\n{}", text);
     Ok(())
 }
