b23d421589b812f701068d314a33567ac4ee9802
Xavier Filippi <gueltir@gmail.com>
2020-03-07 03:23:54 +0100

    Twenty-second day, SECOND STAAAAR (yay)

--- src/days/day22.rs
+++ src/days/day22.rs
@@ -2,194 +2,100 @@ use std::error::Error;
 use std::fs;
 use std::path::Path;
 
-use std::collections::HashSet;
-
-extern crate num_bigint;
-extern crate num_traits;
-
-fn new_stack(stack: &mut [i32], _steps: i32) -> Vec<i32> {
-    stack.reverse();
-    (*stack).to_vec()
-}
-
-fn cut_n_card(stack: &mut [i32], steps: i32) -> Vec<i32> {
-    let pivot = if steps >= 0 {
-        steps as usize
-    } else {
-        (stack.len() as i32 + steps) as usize
-    };
-
-    let result = [&stack[pivot..], &stack[..pivot]].concat();
-
-    result.to_vec()
-}
-
-fn deal_with_n(stack: &mut [i32], steps: i32) -> Vec<i32> {
-    let mut shuffled_deck = stack.to_vec();
-
-    let mut index = 0;
-    let steps_usize = steps as usize;
-    let len = stack.len();
-
-    for elem in stack.iter() {
-        shuffled_deck[index] = *elem;
-        index = (index + steps_usize) % len;
-    }
-    shuffled_deck
-}
-
-type OP = (fn(&mut [i32], i32) -> Vec<i32>, i32);
-
-fn prepare_file(input: String) -> Vec<OP> {
-    let mut ops: Vec<OP> = vec![];
+fn prepare_file(input: String, stack_len: i128) -> (i128, i128) {
+    let mut coef = (1, 0);
 
     for line in input.lines() {
         let splitted: Vec<String> = line.split(' ').map(|elem| elem.to_string()).collect();
-        let attempt_parsing = splitted.last().unwrap().parse::<i32>();
+        let attempt_parsing = splitted.last().unwrap().parse::<i128>();
 
         if let std::result::Result::Ok(value) = attempt_parsing {
             if splitted.len() == 2 {
-                ops.push((cut_n_card, value));
+                coef.1 = coef.1 - value;
             } else {
-                ops.push((deal_with_n, value));
+                coef.0 = coef.0 * value;
+                coef.1 = coef.1 * value;
             }
         } else {
-            ops.push((new_stack, 0));
+            coef.0 = -coef.0;
+            coef.1 = -coef.1 - 1;
         }
+
+        coef.0 %= stack_len;
+        coef.1 %= stack_len;
     }
 
-    ops
-}
-
-pub fn first_star() -> Result<(), Box<dyn Error + 'static>> {
-    let ops = prepare_file(fs::read_to_string(Path::new("./data/day22.txt"))?);
-    let mut deck: Vec<i32> = (0..10_007).collect();
-
-    for (fun, steps) in ops {
-        deck = fun(&mut deck, steps);
-    }
-
-    let result = deck.iter().position(|&x| x == 2019).unwrap_or(0);
-
-    println!("{:?}", result);
-
-    Ok(())
+    coef
 }
 
 // Copied from https://www.csee.umbc.edu/~chang/cs203.s09/exteuclid.shtml, needed http://defeo.lu/in310/poly/euclide-bezout/ and https://www.mathraining.be/chapters/4?type=1&which=16 to understand it
 // Inverse modulo is located in the 3 tuple
-fn extended_euclid(a: i64, b:i64) -> (i64, i64, i64) {
-    if b == 0 {
-        (a, 1, 0)
+fn extended_euclid(max: i128, min:i128) -> (i128, i128, i128) {
+    if min == 0 {
+        (max, 1, 0)
     } else {
-        let (d1, s1, t1) = extended_euclid(b, a%b);
-        (d1, t1, s1 - (a / b) * t1) // See http://defeo.lu/in310/poly/euclide-bezout/ on recursive relation
+        let (d1, s1, t1) = extended_euclid(min, max%min);
+        (d1, t1, s1 - (max / min) * t1) // See http://defeo.lu/in310/poly/euclide-bezout/ on recursive relation
     }
 }
 
-fn reverse_new_stack(stack_len: i64, arrival_pos: i64, _steps: i64) -> i64 {
-    let pivot = stack_len / 2;
-    if arrival_pos == pivot {
-        pivot
-    } else if arrival_pos < pivot {
-        stack_len - arrival_pos - 1
-    } else {
-        -arrival_pos + stack_len - 1
-    }
-}
+// https://en.wikipedia.org/wiki/Exponentiation_by_squaring, provided me wrong results because of the way I initialy handled odd numbered powers, couldn't have guessed without code from here https://github.com/enjmusic/aoc_2019/blob/master/aoc_22/src/main.rs
+fn modular_power(base: i128, power: i128, modulo: i128) -> i128 {
+    let mut total = base % modulo;
+    let mut current_pow = power;
+    let mut temp = 1;
 
-fn reverse_cut_n_card(stack_len: i64, arrival_pos: i64, steps: i64) -> i64 {
-    let absolute_steps = if steps < 0 {
-        stack_len + steps
-    } else {
-        steps
-    };
-
-    (arrival_pos + absolute_steps) % stack_len
-}
-
-fn reverse_deal_with_n(stack_len: i64, arrival_pos: i64, steps: i64) -> i64 {
-    use num_bigint::BigInt;
-    use num_traits::cast::ToPrimitive;
-
-    let (_, _, mut reverse) = extended_euclid(stack_len, steps);
-    while reverse < 0 {
-        reverse += stack_len;
-    }
-
-    let a = BigInt::from(arrival_pos);
-    let b = BigInt::from(reverse);
-    let c = BigInt::from(stack_len);
-
-    let x = (a * b) % c;
-
-    x.to_i64().unwrap_or(0)
-}
-
-type ReverseOp = (fn(i64, i64, i64) -> i64, i64);
-
-fn prepare_file_reverse(input: String) -> Vec<ReverseOp> {
-    let mut ops: Vec<ReverseOp> = vec![];
-
-    for line in input.lines() {
-        let splitted: Vec<String> = line.split(' ').map(|elem| elem.to_string()).collect();
-        let attempt_parsing = splitted.last().unwrap().parse::<i64>();
-
-        if let std::result::Result::Ok(value) = attempt_parsing {
-            if splitted.len() == 2 {
-                ops.push((reverse_cut_n_card, value));
-            } else {
-                ops.push((reverse_deal_with_n, value));
-            }
-        } else {
-            ops.push((reverse_new_stack, 0));
+    while current_pow > 1 {
+        if current_pow % 2 == 1 {
+            temp = (temp * total) % modulo;
+            current_pow -= 1;
         }
+        total = total.pow(2) % modulo;
+        current_pow /= 2;
     }
-    ops.reverse();
-    ops
+    (total * temp) % modulo
 }
 
-pub fn second_star() -> Result<(), Box<dyn Error + 'static>> {
-    let ops = prepare_file_reverse(fs::read_to_string(Path::new("./data/day22.txt"))?);
+pub fn first_star() -> Result<(), Box<dyn Error + 'static>> {
+    let stack_len = 10_007;
+    let coef = prepare_file(fs::read_to_string(Path::new("./data/day22.txt"))?, stack_len);
 
-    let mut already_solved: HashSet<i64> = HashSet::new();
+    let mut result = (2019 * coef.0 + coef.1) % stack_len;
 
-
-    let mut resolved_index: Vec<i64> = Vec::new();
-
-    let stack_len = 119_315_717_514_047i64;
-    let total = 101_741_582_076_661i64;
-    let mut index = 2020;
-    let mut total_done = 0;
-
-    already_solved.insert(index);
-    
-    for count in 0..total {
-        resolved_index.push(index);
-        
-        for (fun, steps) in ops.iter() {
-            index = fun(stack_len, index, *steps);
-            if index < 0 {
-                panic!("Ouch");
-            }
-        }
-
-        if !already_solved.insert(index) {
-            println!("{}", index);
-            total_done = count;
-            break;
-        }
+    if result < 0 {
+        result = stack_len + result;
     }
 
-    let start_point = resolved_index.iter().position(|x| *x == index).unwrap_or(0);
-    resolved_index = resolved_index.into_iter().skip(start_point).collect();
+    println!("From position 2019 to {}", result);
 
-    let remaining_shuffles = ((total - total_done) % resolved_index.len() as i64) as usize;
-
-    println!("I still have {} shuffles to do", remaining_shuffles);
-
-    println!("Result: {}", resolved_index[remaining_shuffles]);
     Ok(())
 }
 
-// 75628569713232 too high
+
+pub fn second_star() -> Result<(), Box<dyn Error + 'static>> {
+    let stack_len = 119_315_717_514_047_i128;
+    let num_of_computations = 101_741_582_076_661_i128;
+
+    let (factor, constant) = prepare_file(fs::read_to_string(Path::new("./data/day22.txt"))?, stack_len);
+
+    let (_, _, denominator_inverse) = extended_euclid(stack_len, 1 - factor);
+
+    let total_multiplier = modular_power(factor, num_of_computations, stack_len);
+
+    // let total_offset = constant * (1 - total_multiplier) / (1 - factor); // Because division doesn't really works with modulo?
+
+    let total_offset = (constant * (((1 - total_multiplier) * denominator_inverse) % stack_len)) % stack_len;
+
+    let (_, _, reverse_multiplier) = extended_euclid(stack_len, total_multiplier); // Note: According to solution, I could also find any reverse by calculing a^b where b is stack_len - 2, since stack_len is prime, any a^(stack_len-1) = 1 mod stack_len
+
+    let mut result = (2020 - total_offset) % stack_len;
+    result = (result * reverse_multiplier) % stack_len;
+
+    if result < 0 {
+        result = stack_len + result;
+    }
+
+    println!("{}", result);
+
+    Ok(())
+}
